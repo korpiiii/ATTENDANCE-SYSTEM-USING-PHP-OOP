cd9664aa0d84368cb3352889bbed0892bdcecf8d
Aloof <167641185+korpiiii@users.noreply.github.com>
2025-09-20 11:41:08 +0800

    Delete includes directory

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-    </main>
-
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> Emilio Aguinaldo College. All rights reserved. 2025</p>
-        </div>
-    </footer>
-</body>
-</html>
